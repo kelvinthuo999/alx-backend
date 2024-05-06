817969757c996c091fe83e397d7ddc95ddc6bed9
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-06 09:44:21 +0000

    create: function to make sure no data is missed

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Return a dictionary with hypermedia pagination information"""
+        assert index is None or (isinstance(index, int) and index >= 0), "Index must be None or a non-negative integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
+
+        indexed_dataset = self.indexed_dataset()
+
+        if index is None:
+            index = 0
+        elif index >= len(indexed_dataset):
+            raise AssertionError("Index out of range")
+
+        data = [indexed_dataset[i] for i in range(index, min(index + page_size, len(indexed_dataset)))]
+
+        next_index = min(index + page_size, len(indexed_dataset))
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(data),
+            'data': data
+        }
