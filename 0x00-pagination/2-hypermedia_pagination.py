617d2679bb360517637dc3adc4f4aa78fb36522e
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-06 09:40:29 +0000

    create: function to return key-value pairs

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Pagination utility for a dataset of popular baby names"""
 
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 import csv
+import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -41,3 +42,19 @@ class Server:
             return data[start:end]
         except IndexError:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
+        """Return hypermedia information for the specified page"""
+        data = self.get_page(page, page_size)
+        next_page = page + 1 if len(data) == page_size else None
+        prev_page = page - 1 if page > 1 else None
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
