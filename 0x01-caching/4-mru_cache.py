e6882047d5df7c718d76d8fa8e292c60eddf9bd2
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-08 05:03:34 +0000

    create: caching class to implement MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""MRUCache module"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache class"""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.queue.pop()
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        if key in self.cache_data:
+            self.queue.remove(key)
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
